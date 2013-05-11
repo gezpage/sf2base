4eb23f0572c9a4bf682127b2efc4f8a0b17a1e70
Gez Page <gezpage@gmail.com>
2013-05-11 14:16:08 +0100

    Puppet augeas added for php.ini config

--- vagrant/puppet/app/toolsextra.pp
+++ vagrant/puppet/app/toolsextra.pp
@@ -8,6 +8,7 @@ class app::toolsextra {
               "git-core",
               "libpcre3-dev",
               "acl",
+              "curl",
               "augeas-tools",
               "postfix"]:
         ensure => present,
@@ -16,6 +17,16 @@ class app::toolsextra {
     exec {"set-apache-permissions":
         require => Package["acl"],
         command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs && setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs'",
+    }
+
+    augeas { 'set-php-ini-values':
+        context => '/etc/php5/apache2/php.ini',
+        changes => [
+           'set Date/date.timezone Europe/London',
+           'set short_open_tag Off',
+        ],
+        require => Package['php5'],
+        notify  => Service['apache2'],
     }
 
 }
